b207881e47a6d54d92a4e6186b5cb1adeb5b8ad9
Ryan Kubiak <rkubiak@google.com>
2017-10-10 16:22:21 -0700

    Periodic release configs

--- script/release.sh
+++ script/release.sh
@@ -7,23 +7,37 @@ set -x
 
 function usage() {
   echo "$0 \
-    -t <tag name to apply to artifacts>"
+    -b <GCS bucket for release artifacts> \
+    -t <tag name to apply to artifacts> \
+    -v <version to apply instead of tag>"
   exit 1
 }
 
 # Initialize variables
+BUCKET="istio-release"
 TAG_NAME=""
+VERSION_OVERRIDE=""
 
 # Handle command line args
-while getopts i:t: arg ; do
+while getopts b:i:t: arg ; do
   case "${arg}" in
+    b) BUCKET="${OPTARG}";;
     t) TAG_NAME="${OPTARG}";;
     *) usage;;
   esac
 done
 
+if [ ! -z "${VERSION_OVERRIDE}" ] ; then
+  version="${VERSION_OVERRIDE}"
+elif [ ! -z "${TAG_NAME}" ] ; then
+  version="${TAG_NAME}"
+else
+  echo "Either -t or -v is a required argument."
+  usage
+fi
+
 script/push-debian.sh \
     -c opt \
-    -v "${TAG_NAME}" \
-    -p "gs://istio-release/releases/${TAG_NAME}/deb"
+    -v "${version}" \
+    -p "gs://${BUCKET}/releases/${version}/deb"
 
